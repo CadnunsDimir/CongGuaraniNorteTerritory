a880c131b821d62c3f84afd4da63c8d12211053a
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2026-03-25 17:44:15 -0300

    fix: removendo variavel não usada

--- core/Environment.js
+++ core/Environment.js
@@ -6,7 +6,6 @@ dotenv.config();
 const Environment = {
     PORT: process.env.PORT || 1234,
     AUTH_SECRET_KEY: process.env.AUTH_SECRET_KEY,
-    SERVICE_ACCOUNT: process.env.SERVICE_ACCOUNT,
     SPREADSHEET_ID: process.env.SPREADSHEET_ID
 }
 
