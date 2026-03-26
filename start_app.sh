84392deb93adba7adcfbaa4c346b1cb825be3e9b
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2026-03-25 23:15:16 -0300

    fix: ajustando deploy para detectar erros na inicialização docker

--- start_app.sh
+++ start_app.sh
@@ -2,8 +2,8 @@
 IMAGE_NAME=congguaraninorte-front
 VPS_NETWORK=vps-network
 
-echo "removendo node modules"
-rm node_modules -r
+# echo "removendo node modules"
+# rm node_modules -r
 
 echo "Limpando versão anterior..."
 docker rm -f $IMAGE_NAME || true
@@ -12,5 +12,9 @@ echo "Contruindo imagem"
 docker build -t $IMAGE_NAME .
 
 echo "Contruindo Container"
-# docker run -d -p 3000:3000 --name $IMAGE_NAME $IMAGE_NAME
+
 docker run -d --network $VPS_NETWORK --name $IMAGE_NAME $IMAGE_NAME
+
+sleep 10
+
+docker logs --tail 20 $IMAGE_NAME
